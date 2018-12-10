1af9b8f05ae327f31af96fe90dac33841b895318
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-12-10 22:05:47 +0100

    Add animation and github logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,17 @@ import styled from 'styled-components'
 
 import Layout from '../components/layout'
 
+import './index.css'
+
 const A = styled.a`
   color: #47ad23;
 `
 
 const IndexPage = () => (
   <Layout>
-    <h1 style={{ textAlign: 'center' }}>
+    <h2 style={{ textAlign: 'center' }}>
       Let's build something great together
-    </h1>
+    </h2>
     <h3 style={{ textAlign: 'center' }}>
       Napisz do nas:{' '}
       <A href="mailto:biuro.tkgf@gmail.com">biuro.tkgf@gmail.com</A>
