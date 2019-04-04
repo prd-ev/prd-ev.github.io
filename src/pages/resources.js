4355171dee446364f02d65d90ac210c71587abbc
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-04-04 21:58:30 +0200

    Add cpp dataset

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import Layout from '../components/layout'
+import TextLink from '../components/TextLink/TextLink'
 import { withPrefix } from 'gatsby'
 import styled from 'styled-components'
 
 const Resource = styled.li`
+  font-size: 24px;
   color: #2fad1f;
-  margin-top: 15px;
+  margin-top: 40px;
   & > a {
     color: #2fad1f;
   }
+`
+
+const Description = styled.p`
+  font-size: 0.9em;
+  text-align: center;
 `
 
 const Section = styled.h3`
@@ -23,10 +30,23 @@ export default () => (
         apple.tar.gz
       </a>
     </Resource>
+    <Description>
+      (W.I.P.) Dataset of apple images from{' '}
+      <TextLink href="https://isthisapple.me" color="#47ad23">
+        isthisapple.me
+      </TextLink>
+    </Description>
     <Resource>
       <a href={withPrefix('/files/rap.tar.gz')} className={'text-link'}>
         rap.tar.gz
       </a>
     </Resource>
+    <Description>Dataset of rap songs</Description>
+    <Resource>
+      <a href={withPrefix('/files/cpp.tar.gz')} className={'text-link'}>
+        cpp.tar.gz
+      </a>
+    </Resource>
+    <Description>Dataset of .cpp files</Description>
   </Layout>
 )
